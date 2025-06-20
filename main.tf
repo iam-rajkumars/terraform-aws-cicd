7baa2423c1e6f818526112a5c16ab6df21ff661f
Stenny17 <stenny.rajkumar@gmail.com>
2025-06-20 19:20:08 +0530

    Fix deprecated ACL and use aws_s3_bucket_acl

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_s3_bucket" "ci_cd_bucket" {
+  bucket = "stenny-ci-cd-demo-bucket-123456"
 }
 
-resource "aws_s3_bucket" "ci_cd_bucket" {
-  bucket = "stenny-ci-cd-demo-bucket-123456"  # Must be globally unique
+resource "aws_s3_bucket_acl" "ci_cd_bucket_acl" {
+  bucket = aws_s3_bucket.ci_cd_bucket.id
   acl    = "private"
 }
 
